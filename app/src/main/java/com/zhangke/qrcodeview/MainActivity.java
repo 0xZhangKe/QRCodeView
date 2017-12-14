9457c75c4f4d47e418a79cac4b8b76032ae01578
张可_711 <zhangke_711@163.com>
2017-12-14 21:15:56 +0800

    添加测试代码

--- app/src/main/java/com/zhangke/qrcodeview/MainActivity.java
+++ app/src/main/java/com/zhangke/qrcodeview/MainActivity.java
@@ -2,12 +2,25 @@ package com.zhangke.qrcodeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QRCodeView mQRCodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mQRCodeView = (QRCodeView) findViewById(R.id.qr_code_view);
+        mQRCodeView.setOnQRCodeListener(new QRCodeView.OnQRCodeRecognitionListener() {
+            @Override
+            public void onQRCodeRecognition(Result result) {
+                Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
