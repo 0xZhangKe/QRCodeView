ee46b2c295bc44ae78b5cbc2869e2597c2fe1d99
张可_711 <zhangke_711@163.com>
2017-12-26 17:14:54 +0800

    添加测试代码

--- sample/src/main/java/com/zhangke/qrcodeview/sample/MainActivity.java
+++ sample/src/main/java/com/zhangke/qrcodeview/sample/MainActivity.java
@@ -1,43 +1,41 @@
 package com.zhangke.qrcodeview.sample;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
-import com.zhangke.qrcodeview.QRCodeView;
+import com.google.zxing.WriterException;
+import com.zhangke.qrcodeview.QRCodeUtil;
 import com.zhangke.qrcodeview.smaple.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
-    private QRCodeView mQRCodeView;
+    private ImageView img01;
+    private ImageView img02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mQRCodeView = (QRCodeView) findViewById(R.id.qr_code_view);
-        mQRCodeView.setOnQRCodeListener(new QRCodeView.OnQRCodeRecognitionListener() {
-            @Override
-            public void onQRCodeRecognition(Result result) {
-                Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.qr_code);
+//        img01.setImageBitmap(bitmap);
+        Toast.makeText(this, QRCodeUtil.decodeQRCode(bitmap), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.e(TAG, "onResume");
-        mQRCodeView.startPreview();
     }
 
     @Override
     protected void onPause() {
-        mQRCodeView.stopPreview();
         super.onPause();
     }
 }
