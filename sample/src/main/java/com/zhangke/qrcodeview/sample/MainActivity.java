f88372200eb8fd0808c3b97ed75c6251029076fe
张可_711 <zhangke_711@163.com>
2018-01-02 16:34:19 +0800

    添加 demo

--- sample/src/main/java/com/zhangke/qrcodeview/sample/MainActivity.java
+++ sample/src/main/java/com/zhangke/qrcodeview/sample/MainActivity.java
@@ -1,16 +1,10 @@
 package com.zhangke.qrcodeview.sample;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
-import com.google.zxing.WriterException;
-import com.zhangke.qrcodeview.QRCodeUtil;
 import com.zhangke.qrcodeview.QRCodeView;
 import com.zhangke.qrcodeview.smaple.R;
 
